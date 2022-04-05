25afe964c2c062350d7485d8862eef4bc8b4cf71
dbrittovivian <dbritto.vivian@gmail.com>
2022-04-04 20:40:32 -0400

    Wordle solver module that will utilize  wordle helper

--- HW03_Vivian_Dbritto_test.py
+++ HW03_Vivian_Dbritto_test.py
@@ -1,73 +1,105 @@
-import unittest
-from HW03_Vivian_Dbritto_ui import Ui as ui
-from HW03_Vivian_Dbritto_wordle import Wordle as wordle
-from HW03_Vivian_Dbritto_dictionary import Dictionary as dictionary
-from HW03_Vivian_Dbritto_letterfrequency import frequency as frequency
-from unittest.mock import patch
+from HW03_Vivian_Dbritto_ui import Ui
+import HW03_Vivian_Dbritto_dictionary as dictionaryClass
+from HW03_Vivian_Dbritto_wordle import Wordle
 
 
-class WordleTest(unittest.TestCase):
-
-    @patch('builtins.input', side_effect=["weeks"])
-    def test_user_input_length_positive(self, mock_inputs) -> None:
-        """Test when the input length is correct"""
-        uitest = ui()
-        self.assertEqual(len(uitest.userinput(1)), 5)
-
-    @patch('builtins.input', side_effect=["burger"])
-    def test_user_input_length_negative(self, mock_inputs) -> None:
-        """Test when the input length is not correct"""
-        uitest = ui()
-        self.assertNotEqual(len(uitest.userinput(1)), 5)
-
-    @patch('builtins.input', side_effect=["walks"])
-    def test_user_input_valid_dictionary_word_positive(self, mock_inputs) -> None:
-        """Test when the input word is a valid dictionary word"""
-        dicttest = dictionary()
-        uitest = ui()
-        self.assertTrue(dicttest.checkWord(uitest.userinput(1)))
-
-    @patch('builtins.input', side_effect=["craft"])
-    def test_user_input_no_special_characters(self, mock_inputs) -> None:
-        """Test when the input does not contain any special characters"""
-        uitest = ui()
-        self.assertTrue(uitest.userinput(1))
-
-    def test_compare_word_function_positive(self) -> None:
-        """Testing the compare function with correct inputs"""
-        wordletest = wordle()
-        self.assertTrue(wordletest.compareWord("Zones", "Zones"))
-
-    def test_compare_word_function_negative(self) -> None:
-        """Testing the compare function with incorrect inputs"""
-        wordletest = wordle()
-        self.assertFalse(wordletest.compareWord("Zones", "Looks"))
-
-    def test_quit_function_positive(self) -> None:
-        """Testing the quit function with empty string"""
-        uitest = ui()
-        self.assertTrue(uitest.quitfunction(""))
-
-    def test_quit_function_negative(self) -> None:
-        """Testing the quit function with a valid word"""
-        uitest = ui()
-        self.assertFalse(uitest.quitfunction("Books"))
-
-    def test_file_size_function(self) -> None:
-        """Testing file size function"""
-        dicttest = dictionary()
-        self.assertFalse(dicttest.fileSize())
-
-    def test_check_temp_function_positive(self) -> None:
-        """Testing checkTemp function with temp as not 0"""
-        frequencyltest = frequency()
-        self.assertTrue(frequencyltest.checkTemp(["A", (3, 5, 7, 2, 3)]))
-
-    def test_check_temp_function_negative(self) -> None:
-        """Testing checkTemp function with temp as 0"""
-        frequencyltest = frequency()
-        self.assertFalse(frequencyltest.checkTemp([""]))
+def test_both_empty():
+    result = Ui.checkInput("sonar")
+    assert(result == True)
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_upper():
+    result = Ui.checkInput("SONAR")
+    assert(result == True)
+
+
+def test_empty():
+    result = Ui.checkInput("")
+    assert(result == False)
+
+
+def test_num():
+    result = Ui.checkInput("12345")
+    assert(result == False)
+
+
+def test_multiple_words():
+    result = Ui.checkInput("Hello World")
+    assert(result == False)
+
+
+def test_word_used_one():
+    result = Ui.checkWordUsed(
+        "sonar".upper(), ["SONAR", "HELLO", "YEMEN", "COULD", "YACHT"])
+    assert(result == False)
+
+
+def test_word_used_two():
+    result = Ui.checkWordUsed(
+        "SONAR".upper(), ["SONAR", "HELLO", "YEMEN", "COULD", "YACHT"])
+    assert(result == False)
+
+
+def test_word_used_three():
+    result = Ui.checkWordUsed(
+        "SHIFT", ["SONAR", "HELLO", "YEMEN", "COULD", "YACHT"])
+    assert(result == True)
+
+
+def test_word_used_four():
+    result = Ui.checkWordUsed(
+        "", ["SONAR", "HELLO", "YEMEN", "COULD", "YACHT"])
+    assert(result == True)
+
+
+def test_word_length_one():
+    result = Ui.checkWordLength(
+        "     ")
+    assert(result == False)
+
+
+def test_word_length_two():
+    result = Ui.checkWordLength(
+        "")
+    assert(result == False)
+
+
+def test_word_length_three():
+    result = Ui.checkWordLength(
+        "SONAR")
+    assert(result == True)
+
+
+def test_word_length_three():
+    result = Ui.checkWordLength(
+        "SON")
+    assert(result == False)
+
+
+def test_word_length_three():
+    result = Ui.checkWordLength(
+        "SONARSONARSONAR")
+    assert(result == False)
+
+
+def test_dict_word_length():
+    todays_word, word_list = dictionaryClass.Dictionary().getRandomWord([
+        "YACHT"])
+    assert(len(todays_word) == 5)
+
+
+def test_word_in_dict():
+    todays_word, word_list = dictionaryClass.Dictionary().getRandomWord([
+        "YACHT"])
+    assert(todays_word in word_list)
+
+
+def test_word_dict_isUpper():
+    todays_word, word_list = dictionaryClass.Dictionary().getRandomWord([
+        "YACHT"])
+    assert(todays_word.isupper())
+
+
+def test_divide_by_zero():
+    game = Wordle()
+    assert(game.displayResults(0, 0, []) == None)
--- HW03_Vivian_Dbritto_wordle.py
+++ HW03_Vivian_Dbritto_wordle.py
@@ -1,111 +1,165 @@
-import HW03_Vivian_Dbritto_ui as ui
-from HW03_Vivian_Dbritto_dictionary import Dictionary as dictionary
-from HW03_Vivian_Dbritto_letterfrequency import frequency as frequency
+from HW03_Vivian_Dbritto_ui import Ui
+import HW03_Vivian_Dbritto_dictionary as dictionaryClass
+from HW03_Vivian_Dbritto_letterfrequency import CalculateStatistics
+from typing import Dict, List, Tuple
+from HW08_Vivian_Dbritto_Word_Finder import Helper
 
 
 class Wordle:
+    def __init__(self) -> None:
+        self.dictionary = dictionaryClass.Dictionary()
 
-    gamesPlayed = 0
-    gamesWon = 0
-    guessDistribution = []
+    def __str__(self) -> None:
+        return "Main Game class"
 
-    def __init__(self):
-        self.ui = ui.Ui()
-        self.dictionary = dictionary()
-        self.gamesPlayed = 0
-        self.gamesWon = 0
-        self.guessDistribution = [0, 0, 0, 0, 0, 0]
-
-    def __str__(self) -> str:
-        return f"Wordle(gamesPlayed:{str(self.gamesPlayed)}, gamesWon:{str(self.gamesWon)}, guessDistribution:{str(self.guessDistribution)})"
-
-    def compareWord(self, a, b):
-        if(a == b):
-            return True
-        else:
-            return False
+    # Initialise all variables
+    def initVariables(self, words_used: List[str]) -> Tuple[str, List, List, str, int, List, str, str]:
+        todays_word, word_list = self.dictionary.getRandomWord(words_used)
+        # print(str(self.dictionary))
+        words_used.append(todays_word)
+        entered_words = []
+        answer = [None, None, None, None, None]
+        attempt = 0
+        good_letters = ""
+        bad_letters = ""
+        # print(todays_word)
+        return todays_word, word_list, entered_words, answer, attempt, words_used, good_letters, bad_letters
 
     def main(self):
-        while(True):
-            lltest = frequency()
-            lltest.letterLikelyhood()
-            f = open("gameplay.log", "a+")
-            self.gamesPlayed = self.gamesPlayed + 1
-            try:
-                answer = self.dictionary.randomWord()
-            except:
-                print("Answer not found")
-            try:
-                self.dictionary.removeWord(answer)
-            except:
-                print("Remove word not working")
-            f.write(f"Selected Word: {answer}\n")
-            attempts = 1
-            while(attempts < 7):
-                word = self.ui.userinput(attempts)
-                status = []
-                while(self.dictionary.checkWord(word) != True):
-                    print("Not a valid dictionary word")
-                    word = self.ui.userinput(attempts)
-                f.write(f"User Input:{word}\n")
-                letter_counts: dict = {}  # making a dictionary to store the number of letters
 
-                if(self.compareWord(word, answer)):
-                    print("Correct Word! \n")
-                    self.gamesWon = self.gamesWon + 1
-                    self.guessDistribution[attempts-1] += 1
-                    print(
-                        "Now the word has been changed, to exit please press ENTER/RETURN")
-                    break
+        words_used = []
+        todays_word, word_list, entered_words, answer, attempt, words_used, good_letters, bad_letters = self.initVariables(
+            words_used)
+        games_played = 0
+        games_won = 0
+        guess_history = []
+
+        while attempt < 6:
+            # input_word = Ui.inputFromUser(attempt)
+            new_answer = ""
+            if all(v is not None for v in answer):
+                for char in answer:
+                    if char == "`" or char == '"':
+                        new_answer += " "
+                    else:
+                        new_answer += char
+            input_word = Helper.help(
+                good_letters, bad_letters, new_answer, entered_words)
+
+            print(f"Attempt {attempt+1} : {input_word}\n")
+
+            # if empty word entered then quit playing
+            if(len(input_word.strip()) == 0):
+                print("\nThank you for Playing.!")
+                print("\nExiting")
+                self.displayResults(games_played, games_won, guess_history)
+                break
+
+            # check if entered word is having any special characters
+            if(not Ui.checkInput(input_word)):
+                continue
+
+            # check if entered word has already been entered before
+            if(not Ui.checkWordUsed(input_word, entered_words)):
+                continue
+
+            # check if entered word is of length 5
+            if(not Ui.checkWordLength(input_word)):
+                continue
+
+            # check if word was entered previously
+            if(input_word not in word_list):
+                print("\nEntered word not in dictionary!")
+                continue
+
+            # check if the guess is correct
+            if input_word == todays_word:
+                print(
+                    f"Hurray !!!\nYou correctly guessed today's word: {todays_word}")
+
+                games_played += 1
+                games_won += 1
+                guess_history.append(attempt+1)
+                self.displayResults(games_played, games_won, guess_history)
+                todays_word, word_list, entered_words, answer, attempt, words_used, good_letters, bad_letters = self.initVariables(
+                    words_used)
+                # print("Starting a New Game, Press enter to exit")
+                # continue
+                break
+
+            letter_counts = {}
+
+            # Generate a dictionary to store letter count
+            for letter in todays_word:
+                if letter in letter_counts.keys():
+                    letter_counts[letter] += 1
                 else:
-                    for letter in answer:  # loop used to store the number of letters in the answer
-                        if letter in letter_counts.keys():
-                            letter_counts[letter] += 1
-                        else:
-                            letter_counts[letter] = 1
-                    for index in range(len(answer)):  # loop used to store the status
-                        if word[index] == answer[index]:
-                            status.append(' ')
-                            letter_counts[answer[index]] -= 1
-                        else:
-                            status.append('"')
+                    letter_counts[letter] = 1
 
-                    for index in range(len(answer)):
-                        if word[index] != answer[index]:
-                            if word[index] in letter_counts:
-                                if letter_counts[word[index]] > 0:
-                                    letter_counts[word[index]] -= 1
-                                    status[index] = "`"
-                    attempts = attempts + 1
-                    # printing the status after an attempt
-                    print(''.join(status))
-                if(attempts == 7):  # checking if this is the last attempt or not
-                    print(f"The correct answer is #{answer}")
-                    print(
-                        "Now the word has been changed, to exit please press ENTER/RETURN")
-            print("GAME STATISTICS")
-            print(f"Total number of games played: {self.gamesPlayed}")
+            # comparing the characters of input_word and todays_word in corresponding position
+            for index in range(len(input_word)):
+                if input_word[index] == todays_word[index]:
+                    answer[index] = todays_word[index]
+                    letter_counts[todays_word[index]] -= 1
+                    if input_word[index] not in good_letters:
+                        good_letters += input_word[index]
+                else:
+                    answer[index] = '"'
+
+            # checking which characters of input_word are present and absent in todays_word
+            for index in range(len(input_word)):
+                if input_word[index] != todays_word[index]:
+                    if input_word[index] in letter_counts:
+                        if letter_counts[input_word[index]] > 0:
+                            letter_counts[input_word[index]] -= 1
+                            answer[index] = "`"
+                            if input_word[index] not in good_letters:
+                                good_letters += input_word[index]
+                    else:
+                        if input_word[index] not in bad_letters:
+                            bad_letters += input_word[index]
+
+            entered_words.append(input_word)
+            attempt += 1
+
+            print(f"The good letters selected: " + good_letters)
+            print(f"The bad letters selected : " + bad_letters + "\n")
+
+            if attempt == 6:
+                print(f"Hard luck!!!\nToday's word was {todays_word}")
+                games_played += 1
+                self.displayResults(games_played, games_won, guess_history)
+                todays_word, word_list, entered_words, answer, attempt, words_used, good_letters, bad_letters = self.initVariables(
+                    words_used)
+                # print("Starting a New Game, Press enter to exit")
+                break
+
+    def displayResults(self, games_played: int, games_won: int, guess_history: List[str]) -> None:
+        print(f"\nNumber of Games Played : {games_played}")
+        print(f"Guess distribution : {guess_history}")
+
+        try:
+            print(f"Win % : {(games_won/games_played)*100}")
+        except ZeroDivisionError:
+            print('Win % : 0')
+
+        try:
+            f = open("gameplay.log", "a")
+            f.write((f"Number of Games Played : {games_played}\n"))
+            f.write(f"Guess distribution : {guess_history}\n")
+            f.write(f"Win % : {(games_won/games_played)*100}\n\n")
+            f.close()
+        except IOError:
+            print('An error occured trying to read the file.')
             print(
-                f"Win percentage: {(self.gamesWon*100/self.gamesPlayed): .2f}%")
-            print("Guess Distribution")
-            print(f"Guessed in 1st attempt : {self.guessDistribution[0]}")
-            print(f"Guessed in 2nd attempt : {self.guessDistribution[1]}")
-            print(f"Guessed in 3rd attempt : {self.guessDistribution[2]}")
-            print(f"Guessed in 4th attempt : {self.guessDistribution[3]}")
-            print(f"Guessed in 5th attempt : {self.guessDistribution[4]}")
-            print(f"Guessed in 6th attempt : {self.guessDistribution[5]}")
-            f.write("User Report\n")
-            f.write(f"Total number of games played: {self.gamesPlayed}\n")
-            f.write(
-                f"Win percentage: {(self.gamesWon*100/self.gamesPlayed): .2f}%\n")
-            f.write(f"Guessed in 1st attempt : {self.guessDistribution[0]}\n")
-            f.write(f"Guessed in 2nd attempt : {self.guessDistribution[1]}\n")
-            f.write(f"Guessed in 3rd attempt : {self.guessDistribution[2]}\n")
-            f.write(f"Guessed in 4th attempt : {self.guessDistribution[3]}\n")
-            f.write(f"Guessed in 5th attempt : {self.guessDistribution[4]}\n")
-            f.write(f"Guessed in 6th attempt : {self.guessDistribution[5]}\n")
+                'Please make sure "gameplay.log" is present in the directory before running the program')
+            quit()
+        except ZeroDivisionError:
+            f.write(f"Win % : 0\n\n")
 
 
 if __name__ == "__main__":
-    wordle = Wordle()
-    wordle.main()
+    game = Wordle()
+    game.main()
+    stats = CalculateStatistics()
+    stats.calculateAndSortWords()
